756d6abe5ab1f32027b521351de3afd76813a47e
Student <student@Student-43.lan>
2017-09-06 08:32:45 +0800

    Initial commit1

---
+++ app/src/main/java/cn/edu/gdme/android/mobile1/mobile1.java
@@ -0,0 +1,13 @@
+package cn.edu.gdme.android.mobile1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mobile1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mobile1);
+    }
+}
